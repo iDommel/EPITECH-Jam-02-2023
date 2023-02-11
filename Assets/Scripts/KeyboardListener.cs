0da981ba110cbdf52bd79ed9adb39eb3cb447a91
Iona Dommel-Prioux <iona.dommel-prioux@epitech.eu>
2023-02-11 16:17:44 +0100

    ✨ ADDED: scene names

--- Assets/Scripts/KeyboardListener.cs
+++ Assets/Scripts/KeyboardListener.cs
@@ -6,11 +6,11 @@ public class KeyboardListener : MonoBehaviour
 {
     public float speed = 10.0f;
 
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rigidbodyname;
 
     private void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbodyname = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -20,6 +20,6 @@ public class KeyboardListener : MonoBehaviour
 
         Vector2 velocity = new Vector2(horizontal, vertical) * speed;
 
-        rigidbody2D.velocity = velocity;
+        rigidbodyname.velocity = velocity;
     }
 }
